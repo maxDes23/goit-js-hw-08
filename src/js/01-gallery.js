53a438594f3a2704f5f4fb99f865a1182fd65ae4
maxDes23 <133263504+maxDes23@users.noreply.github.com>
2023-10-08 21:55:44 +0300

    dz8(1)

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,28 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
+import { galleryItems } from './gallery-items.js';
 
-console.log(galleryItems);
+import SimpleLightbox from 'simplelightbox';
+
+import 'simplelightbox/dist/simple-lightbox.min.css';
+const gallery = document.querySelector('.gallery');
+const galleryMarkup = createGallery(galleryItems);
+gallery.insertAdjacentHTML('beforeend', galleryMarkup);
+
+function createGallery(items) {
+  return items
+    .map(({ preview, original, description }) => {
+      return `
+      <li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}"  data-source="${original}" alt="${description}" />
+   </a>
+</li>
+      `;
+    })
+    .join('');
+}
+
+new SimpleLightbox('.gallery__link', {
+  navText: ['←', '→'],
+  captionsData: 'alt',
+  captionDelay: 250,
+});
