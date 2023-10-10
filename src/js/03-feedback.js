390b4f16693194fcc27e943a41185f716c65e75b
maxDes23 <133263504+maxDes23@users.noreply.github.com>
2023-10-10 21:42:22 +0300

    Update 03-feedback.js
    dz

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,8 +1,7 @@
-import throttle from 'lodash.throttle';
-
 const form = document.querySelector('.feedback-form');
 const emailInput = form.querySelector('input[name="email"]');
 const messageTextarea = form.querySelector('textarea[name="message"]');
+const submitButton = form.querySelector('button[type="submit"]');
 
 function saveFormData(event) {
   const formData = {
@@ -38,9 +37,13 @@ form.addEventListener('submit', function (event) {
     message: messageTextarea.value,
   };
 
-  console.log(formData);
+  if (formData.email.trim() !== '' && formData.message.trim() !== '') {
+    console.log(formData);
 
-  localStorage.removeItem('feedback-form-state');
-  emailInput.value = '';
-  messageTextarea.value = '';
+    localStorage.removeItem('feedback-form-state');
+    emailInput.value = '';
+    messageTextarea.value = '';
+  } else {
+    alert('Будь ласка, заповніть обидва поля форми перед відправленням.');
+  }
 });
